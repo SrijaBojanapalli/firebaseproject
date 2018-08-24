0a6f4d4cb4e8fb3ef54d7fd5b63ba06d40aafbcc
HealthSignz <HealthSignz>
2018-08-24 17:59:26 +0530

    firebase project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
+import {ShareService} from '../app/share.service';
+
+export const appRoutes: Routes = [
+  { path: 'create',
+    component: CreateComponent
+  },
+  { path: 'index',
+    component: IndexComponent
+  }
+];
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [ShareService],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
